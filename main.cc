0c2d534e8466c16c1001427967bdbc1d17177f36
kay <wolflikai@163.com>
2021-09-28 21:16:50 +0800

    init

---
+++ main.cc
@@ -0,0 +1,15 @@
+/*
+ * @Author: kay1ess
+ * @Date: 2021-09-28 21:07:36 
+ * @Last Modified by: kay1ess
+ * @Last Modified time: 2021-09-28 21:14:55
+ */
+
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "hls2flv\n";
+}
